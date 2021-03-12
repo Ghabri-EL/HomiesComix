abc819475f5712c075f8d8a56f6b7ff2aaf027f9
Ghabri-EL <gherasimg15@gmail.com>
2021-03-12 13:06:33 +0000

    Created login_signup page and database for users and admins/owners

---
+++ src/main/java/app/User.java
@@ -0,0 +1,91 @@
+package app;
+
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String surname;
+
+    @Column
+    private String address;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    //@OneToMany(mappedBy = "user")
+    //private List<Order> orders;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // public List<Order> getOrders() {
+    //     return this.orders;
+    // }
+
+    // public void setOrders(List<Order> orders) {
+    //     this.orders = orders;
+    // }
+
+}
